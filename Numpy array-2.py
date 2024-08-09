f61ce3f1f690464e7a713508a00df667c71208ae
Devika131 <yadavdevika131@gmail.com>
2024-08-09 13:15:04 +0530

    Add files via upload

---
+++ Numpy array-2.py
@@ -0,0 +1,8 @@
+import numpy as np
+my_list = [1, 2, 3, 4, 5]
+my_array = np.array(my_list)
+print("display array:", my_array)
+print("First index :", my_array[0])
+print("Last index :", my_array[-1])
+multi_array = my_array * 2
+print("Array after multiplication by 2:", multi_array)
